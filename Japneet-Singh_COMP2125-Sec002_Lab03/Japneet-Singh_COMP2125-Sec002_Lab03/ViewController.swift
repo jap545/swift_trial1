532cc8ba71fd39a137f52d9a8596759d70d04062
jap545 <113646201+jap545@users.noreply.github.com>
2023-07-24 17:28:22 -0400

    Add files via upload

---
+++ Japneet-Singh_COMP2125-Sec002_Lab03/Japneet-Singh_COMP2125-Sec002_Lab03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Japneet-Singh_COMP2125-Sec002_Lab03
+//
+//  Created by user244600 on 7/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
